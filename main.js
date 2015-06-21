7fc25a3130ccbd37953a0da5b4d912d88fa9c378
Jonas Oscarsson <jonas.osc@rsson.nu>
2015-06-21 16:41:37 +0200

    Add player shoot

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function() {
+  var ENGINE_THRUST = 0.2;
   var SIZE = 750;
   var TICK_LENGTH = 40;
 
@@ -14,8 +15,9 @@
     pos: [SIZE/2, SIZE/2],
     dir: 0,
     vel: [0, 0],
-    size: 15
+    size: 15,
   };
+  var projectile;
   var lastStamp;
 
   ctx.fillStyle = "black";
@@ -24,6 +26,7 @@
   var render = function() {
     ctx.fillRect(0, 0, SIZE, SIZE);
     renderPlayer();
+    renderProjectile();
   };
 
   var renderPlayer = function() {
@@ -31,12 +34,21 @@
     ctx.translate(player.pos[0], player.pos[1]);
     ctx.rotate(player.dir + Math.PI/2);
     var path = new Path2D();
-
     path.moveTo(0, -player.size);
     path.lineTo(-player.size/2, player.size);
     path.lineTo(player.size/2, player.size);
     path.lineTo(0, -player.size);
     ctx.stroke(path);
+    ctx.restore();
+  };
+
+  var renderProjectile = function() {
+    if (!projectile) return;
+    ctx.save();
+    ctx.fillStyle = "white";
+    var path = new Path2D();
+    path.arc(projectile.pos[0], projectile.pos[1], 2, 0, Math.PI*2, true);
+    ctx.fill(path);
     ctx.restore();
   };
 
@@ -66,11 +78,13 @@
   });
 
   var calculate = function() {
-    movePlayer();
-    wrapAround();
+    playerMove();
+    playerShoot();
+    wrapAround(player.pos);
+    if (projectile) wrapAround(projectile.pos);
   };
 
-  var movePlayer = function() {
+  var playerMove = function() {
     var rotMul = 0;
 
     if (keyState.left) rotMul = -1;
@@ -78,10 +92,9 @@
     player.dir += rotMul * 0.1;
 
     if (keyState.up) {
-      var thrust = 0.2;
       var thrustVec = [
-        thrust * Math.cos(player.dir),
-        thrust * Math.sin(player.dir),
+        ENGINE_THRUST * Math.cos(player.dir),
+        ENGINE_THRUST * Math.sin(player.dir),
       ];
       player.vel[0] += thrustVec[0];
       player.vel[1] += thrustVec[1];
@@ -91,11 +104,33 @@
     player.pos[1] += player.vel[1];
   };
 
-  var wrapAround = function() {
-    if (player.pos[0] < 0) player.pos[0] += SIZE;
-    if (player.pos[1] < 0) player.pos[1] += SIZE;
-    if (player.pos[0] > SIZE) player.pos[0] -= SIZE;
-    if (player.pos[1] > SIZE) player.pos[1] -= SIZE;
+  var playerShoot = function() {
+    if (projectile) {
+      projectile.reach -= 1;
+      if (projectile.reach == 0) projectile = null;
+      else {
+        projectile.pos[0] += projectile.vel[0];
+        projectile.pos[1] += projectile.vel[1];
+      }
+    } else if (keyState.space) {
+      projectile = {
+        pos: [player.pos[0], player.pos[1]],
+        vel: [
+          12 * Math.cos(player.dir),
+          12 * Math.sin(player.dir)
+        ],
+        reach: 30
+      }
+      projectile.pos[0] += 1.1 * projectile.vel[0];
+      projectile.pos[1] += 1.1 * projectile.vel[1];
+    }
+  };
+
+  var wrapAround = function(pos) {
+    if (pos[0] < 0) pos[0] += SIZE;
+    if (pos[1] < 0) pos[1] += SIZE;
+    if (pos[0] > SIZE) pos[0] -= SIZE;
+    if (pos[1] > SIZE) pos[1] -= SIZE;
   };
 
   var main = function(stamp) {
